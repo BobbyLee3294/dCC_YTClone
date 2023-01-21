59451c3f939dbd527b377e0ffe15b0117f69c295
Bobby Lee <thayungbobby@gmail.com>
2023-01-21 00:10:49 -0500

    changed from user_comments to get_all_comments

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from comments import views
 
 urlpatterns = [
-    path('', views.user_comments),
+    path('', views.get_all_comments),
     path('all', views.get_comments_by_video_id),
 ]
