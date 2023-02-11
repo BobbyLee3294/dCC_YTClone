261dc3747cba2a9e1783f87c729d09736d0fc3e3
Bobby Lee <thayungbobby@gmail.com>
2023-02-11 06:59:08 -0500

    replaced with card layout

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,50 +1,23 @@
 import React from "react";
-
-const VideoPlayer = () => {
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player("existing-iframe", {
-      events: {
-        onReady: onPlayerReady,
-        onStageChange: onPlayerStageChange,
-      },
-    });
-  }
-
-  function onPlayerReady(e) {
-    document.getElementById("existing-iframe").style.borderColor = "#FF6D00";
-  }
-  function changeBorderColor(playerstatus) {
-    let color;
-    if (playerstatus == -1) {
-      color = "#34747F";
-    } else if (playerstatus == 0) {
-      color = "#FFFF00";
-    } else if (playerstatus == 1) {
-      color = "#33691E";
-    } else if (playerstatus == 2) {
-      color = "#DD2C00";
-    } else if (playerstatus == 3) {
-      color = "#AA00FF";
-    } else if (playerstatus == 5) {
-      color = "#FF6D00";
-    }
-    if (color) {
-      document.getElementById("existing-iframe").style.borderColor - color;
-    }
-  }
-  function onPlayerStageChange(e) {
-    changeBorderColor(e.data);
-  }
+import { Link } from "react-router-dom";
+export default function VideoPlayer({ setSavedData }) {
   return (
-    <>
-      <iframe
-        id="existing-iframe"
-        width="630"
-        height="360"
-        src="https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1"
-        style={{ border: "solid 4px #3747F" }}
-      ></iframe>
-    </>
+    <div className="cards">
+      {setSavedData.map((items, snippet) => (
+        <div className="card" key={items.id}>
+          <div className="img-wrapper">
+            <img
+              src={`https://www.youtube.com/watch?v=${items.id.videoID}/sddefault.jpg`}
+              alt={snippet.title}
+            ></img>
+          </div>
+          <div>
+            <h3>{snippet.title}</h3>
+            <p>{snippet.description.silce(0, 100) + "..."}</p>
+            <Link to={`/videos/${items.id}`}>Watch Here...</Link>
+          </div>
+        </div>
+      ))}
+    </div>
   );
-};
-export default VideoPlayer;
+}
