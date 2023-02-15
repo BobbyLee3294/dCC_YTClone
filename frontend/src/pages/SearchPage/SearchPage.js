ccfeb34f262fe06b57b7b94371d10f0e7bb8b1e5
Bobby Lee <thayungbobby@gmail.com>
2023-02-15 14:23:15 -0500

    added ResultsTable and VideoPlayer components

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
-import { KEY } from "../../localKey";
+import { KEY } from "../../YTAPIKey";
 
 //import components
+import ResultsTable from "../../components/ResultsTable/ResultsTable";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
 const SearchPage = () => {
   //this page is used to set up our Axios requests to fetch videos from the YouTube API based on the provided search string
@@ -22,12 +25,23 @@ const SearchPage = () => {
       let response = await axios.get(
         `https://www.googleapis.com/youtube/v3/search?q=${queryParam}&key=${KEY}&part=snippet&type=video`
       );
+      console.log(response.data);
       setSavedData(response.data);
+      console.log(savedData);
     } catch (error) {
       console.log(error.message);
     }
   }
-  return <SearchBar setQueryParam={setQueryParam} />;
+  return (
+    <div id="container">
+      <SearchBar
+        setQueryParam={setQueryParam}
+        ParentSavedData={savedData}
+        getVideo={getVideo}
+      />
+      <ResultsTable savedData={savedData} />
+    </div>
+  );
 };
 
 export default SearchPage;
