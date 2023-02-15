805334e63935a91fc0c3209c333f7329dade1c7f
Bobby Lee <thayungbobby@gmail.com>
2023-02-15 14:36:23 -0500

    commented TODO 😢 and added p tag

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,36 +9,28 @@ const HomePage = () => {
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  // const [cars, setCars] = useState([]);
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchBookshelves = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
+        let response = await axios.get("");
+      } catch (error) {}
     };
-    fetchCars();
-  }, [token]);
-  return (
-    <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {/* <Link to="/addcar">Add Car!</Link>
+    return (
+      <div className="container">
+        <h1>Home Page</h1>
+        <p>How are you doing today, {user.username}</p>
+        {/* <Link to="/addcar">Add Car!</Link>
       {cars &&
         cars.map((car) => (
           <p key={car.id}>
             {car.year} {car.model} {car.make}
           </p>
         ))} */}
-      //TODO #1: place bookshelves here
-    </div>
-  );
+        {/* TODO #1: place bookshelves here*/}
+      </div>
+    );
+  });
 };
-
 export default HomePage;
