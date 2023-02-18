9b4b57bac6d18f979f66776ca09e990a56c44b27
Bobby Lee <thayungbobby@gmail.com>
2023-02-18 09:04:43 -0500

    created ResultCard

---
+++ frontend/src/components/ResultCard/ResultCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function ResultCard({ parentSavedData }) {
+  return (
+    <div className="cards">
+      {parentSavedData.map((el, index) => (
+        <div className="card" key={index}>
+          <div className="img-wrapper">
+            <img
+              src={`https://i.ytimg.com/vi/${el.id.videoId}/default.jpg`}
+              alt={el.snippet.title}
+            ></img>
+          </div>
+          <div>
+            <h3>{el.snippet.title}</h3>
+            <p>{el.snippet.description.slice(0, 200) + "..."}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
