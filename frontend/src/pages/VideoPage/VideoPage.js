51501917978351cef1d2b2225cd3574f1f4a4556
Bobby Lee <thayungbobby@gmail.com>
2023-02-20 20:58:49 -0500

    useParams for videoId & added planning comments

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
+import { useParams } from "react-router-dom";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
 const VideoPage = () => {
+  const { videoId } = useParams();
+
+  //create a
   useEffect(() => {
     //playVideo or something that starts the VideoPlayer component
   });
   //inialize playVideo()
   return (
     <>
-      <VideoPlayer />
+      <VideoPlayer videoId={videoId} />
       {}
     </>
   );
