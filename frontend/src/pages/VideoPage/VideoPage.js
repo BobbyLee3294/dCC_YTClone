cd1024c04a2d144d160fe71f9f14a50e73128537
Bobby Lee <thayungbobby@gmail.com>
2023-02-21 09:59:29 -0500

    removed unecessary comment

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -1,25 +1,43 @@
 import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
+import axios from "axios";
 import { useParams } from "react-router-dom";
+
+import CommmentForm from "../../components/CommentForm/CommentForm";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
 const VideoPage = () => {
   const { videoId } = useParams();
+  const baseURL = `http://127.0.0.1:8000/api/comments/?video_id=${videoId}`;
+  const [comments, setComments] = useState("");
+  const [user, token] = useAuth();
 
-  //create a
   useEffect(() => {
-    //playVideo();
-  });
-
-  //function playVideo(){};
-
+    const getComments = async () => {
+      try {
+        let response = await axios.get(baseURL, {
+          headers: {
+            Authorization: "Bearer: " + token,
+          },
+        });
+        setComments(response.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+    getComments();
+  }, []);
   //initialize comment component
   return (
-    <>
-      <VideoPlayer videoId={videoId} />
-      {}
-    </>
+    <div>
+      <div>
+        <div>
+          <VideoPlayer videoId={videoId} />
+        </div>
+        <div>{/* <CommmentForm comments={comments} /> */}</div>
+      </div>
+    </div>
   );
 };
 
