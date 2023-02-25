e239ae5f14c9f733edd06019e1686d0e29c70cfa
Bobby Lee <thayungbobby@gmail.com>
2023-02-25 16:30:21 -0500

    removed postComment function & associated syntax

--- frontend/src/components/CommentForm/CommentForm.jsx
+++ frontend/src/components/CommentForm/CommentForm.jsx
@@ -3,45 +3,17 @@ import { React, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import useCustomForm from "../../hooks/useCustomForm";
 
-let initalValues = {
-  user: "",
-  text: "",
-  videoId: "",
-};
 const CommentForm = (props) => {
-  const [comment, setComment] = useState("");
-  const [user, token] = useAuth();
-  const [formData = props.postComment(), handleInputChange, handleSubmit] =
-    useCustomForm(initalValues, comment);
-  //initialize postComment component
-  function postComment() {
-    axios
-      .post(`http://127.0.0.1:8000/api/comments/`, formData, {
-        headers: {
-          Authorization: "Bearer: " + token,
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        setComment(response.data);
-        console.log(response.data);
-      });
-    let newComment = {
-      commentName: user.username,
-      commentBody: comment,
-      videoId: props.videoId,
-    };
-    setComment(newComment);
-  }
+  const [user] = useAuth();
   return (
     <div className="container">
-      <form id="comment-form" onSubmit={handleSubmit}>
+      <form id="comment-form" onSubmit={props.handleSubmit}>
         <div className="mb-1 row">
           <label htmlFor="commentName" className="col-sm-1">
             Created by
           </label>
           <div className="col-sm-11">
-            <input type="text" id="userName" value={user.username} />
+            <h1>{user.username}</h1>
             <div className="mb-auto row">
               <label htmlFor="commentBody" className="col-sm-1">
                 Comment
@@ -50,9 +22,9 @@ const CommentForm = (props) => {
                 <textarea
                   id="commentBody"
                   placeholder="Write something.."
-                  value={comment}
+                  value={props.comment}
                   maxLength="500"
-                  onChange={(e) => handleInputChange}
+                  onChange={props.handleInputChange}
                 />
               </div>
             </div>
