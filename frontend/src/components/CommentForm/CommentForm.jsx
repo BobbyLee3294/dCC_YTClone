5a2f7c8bb98c56337a5483b3fdaba4023db87f67
Bobby Lee <thayungbobby@gmail.com>
2023-02-25 13:11:42 -0500

    intialized newComment; Identifier expected

--- frontend/src/components/CommentForm/CommentForm.jsx
+++ frontend/src/components/CommentForm/CommentForm.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import { default as React, default as React, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
 const CommentForm = (props) => {
-  // create a function that will take the comment state and pass it down to this component to return it in this form
-  return {
-    /* */
-  };
+  const [comment, setComment] = useState("");
+  const [user, token] = useAuth();
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newComment = {
+      commentName: user.username,
+      commentBody: comment,
+      videoId: props.videoId,
+    };
+    /* Question: Where do we send this to?*/
+    /* Question: How do we post newComment*/
+  }
+  return (
+    <div className="container">
+      <form id="comment-form" onSubmit={handleSubmit}>
+        <div className="mb-1 row">
+          <label htmlFor="commentName" className="col-sm-1">
+            Created by
+          </label>
+          <div className="col-sm-11">
+            <input type="text" id="userName" value={user.username} />
+            <div className="mb-auto row">
+              <label htmlFor="commentBody" className="col-sm-1">
+                Comment
+              </label>
+              <div className="col-sm-11">
+                <textarea
+                  id="commentBody"
+                  placeholder="Write something.."
+                  value={comment}
+                  maxLength="500"
+                  onChange={(e) => setComment(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CommentForm;
