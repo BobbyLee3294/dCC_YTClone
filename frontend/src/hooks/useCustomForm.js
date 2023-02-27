26a2e64bf81cb24f518b356dd29d28bdb7f1766e
Bobby Lee <thayungbobby@gmail.com>
2023-02-27 16:06:06 -0500

    restored back to starter

--- frontend/src/hooks/useCustomForm.js
+++ frontend/src/hooks/useCustomForm.js
@@ -5,12 +5,7 @@ const useCustomForm = (initialValues = {}, onSubmit) => {
 
   const handleInputChange = (e) => {
     e.persist();
-    // if (e.target.name === "isStudent") {
-    //   setFormValues({ ...formData, [e.target.name]: e.target.checked });
-    // } else {
-    //   setFormValues({ ...formData, [e.target.name]: e.target.value });
-    // };
-    if (e.target.name === "isActive") {
+    if (e.target.name === "isStudent") {
       setFormValues({ ...formData, [e.target.name]: e.target.checked });
     } else {
       setFormValues({ ...formData, [e.target.name]: e.target.value });
