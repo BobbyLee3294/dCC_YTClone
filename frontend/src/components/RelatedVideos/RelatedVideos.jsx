d9d8970c2ef948fc44ab99d85ed14283b394f30a
Bobby Lee <thayungbobby@gmail.com>
2023-03-07 21:35:13 -0500

    copypasta from ResultCard component

--- frontend/src/components/RelatedVideos/RelatedVideos.jsx
+++ frontend/src/components/RelatedVideos/RelatedVideos.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const RelatedVideos = () => {
+const RelatedVideos = ({ related }) => {
   return (
     <div>
-      <h1>Related Videos</h1>ks
+      <h1>Related Videos</h1>
+      {Object.values(related).map((el, index) => (
+        <div className="card" key={index}>
+          <Link to={`${el.id.videoId}`}>
+            <div className="img-wrapper">
+              <img
+                src={`https://i.ytimg.com/vi/${el.id.videoId}/default.jpg`}
+                alt={el.snippet.title}
+              ></img>
+            </div>
+            <div>
+              <h3>{el.snippet.title}</h3>
+              <p>{el.snippet.description.slice(0, 200) + "..."}</p>
+            </div>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
