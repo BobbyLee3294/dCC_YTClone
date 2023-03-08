b2394066b68e1ade978bbdd15a1a3f488800d990
Bobby Lee <thayungbobby@gmail.com>
2023-03-07 20:22:26 -0500

    added title and description text

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -2,13 +2,21 @@ import React from "react";
 
 const VideoPlayer = (props) => {
   return (
-    <iframe
-      id="ytplayer"
-      type="text/html"
-      width="640"
-      height="360"
-      src={`https://www.youtube.com/embed/${props.videoId}?autoplay=1&origin=http://example.com`}
-    ></iframe>
+    <div>
+      <div>
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          width="640"
+          height="360"
+          src={`https://www.youtube.com/embed/${props.videoId}?autoplay=1&origin=http://example.com`}
+        />
+      </div>
+      <div>
+        <h1>Title of Video</h1>
+        <p>Description of Video</p>
+      </div>
+    </div>
   );
 };
 
