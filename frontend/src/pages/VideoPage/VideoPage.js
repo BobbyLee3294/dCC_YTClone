ef9976c80ee62e8133f1260e396553f756b35fdf
Bobby Lee <thayungbobby@gmail.com>
2023-03-07 21:34:35 -0500

    added RelatedVideos component; created getRelatedVideos; created related state variable

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from "react";
-import useAuth from "../../hooks/useAuth";
-
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import { KEY } from "../../YTAPIKey";
 
 import CommmentForm from "../../components/CommentForm/CommentForm";
 import CommentList from "../../components/CommentList/CommentList";
+import RelatedVideos from "../../components/RelatedVideos/RelatedVideos";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
 const VideoPage = () => {
   const { videoId } = useParams();
   const baseURL = `http://127.0.0.1:8000/api/comments/${videoId}/`;
+  const relatedURL = `https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoId}&part=snippet&type=video&key=${KEY}`;
   const [comments, setComments] = useState([]);
+  const [related, setRelated] = useState([]);
   const [formComment, setFormComment] = useState("");
   const [user, token] = useAuth();
 
   useEffect(() => {
     getCommentsByVideoId();
+    getRelatedVideos();
   }, []);
 
   async function getCommentsByVideoId() {
@@ -35,7 +39,16 @@ const VideoPage = () => {
       console.log(error.message);
     }
   }
-
+  async function getRelatedVideos() {
+    try {
+      axios.get(relatedURL).then((response) => {
+        console.log("Related Videos retrived.");
+        setRelated(response.data.items);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   //initialize postComment function which will also be used as a handleSubmit handler for CommentForm
   async function postComment(e) {
     e.preventDefault();
@@ -64,6 +77,9 @@ const VideoPage = () => {
       <div>
         <div>
           <VideoPlayer videoId={videoId} />
+        </div>
+        <div>
+          <RelatedVideos related={related} />
         </div>
         <div>
           <CommmentForm
